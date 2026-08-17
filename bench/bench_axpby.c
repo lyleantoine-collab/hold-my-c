804bb8eb0373d5b2e7255e2f17fe114c4c508101
Lyle Antoine <lyleantoine@gmail.com>
2026-08-17 00:30:01 -0230

    Create bench_axpby.c

---
+++ bench/bench_axpby.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "../src/axpby.h"
+
+#define N 10000000
+#define ITERATIONS 1000
+
+int main() {
+    float alpha = 2.0f;
+    float beta = 0.5f;
+
+    float *x = (float *)malloc(N * sizeof(float));
+    float *y = (float *)malloc(N * sizeof(float));
+
+    if (!x || !y) {
+        fprintf(stderr, "Allocation failed\n");
+        return 1;
+    }
+
+    for (int i = 0; i < N; i++) {
+        x[i] = 1.0f;
+        y[i] = 2.0f;
+    }
+
+    clock_t start = clock();
+    for (int it = 0; it < ITERATIONS; it++) {
+        axpby_scalar(N, alpha, x, beta, y);
+    }
+    clock_t end = clock();
+
+    double elapsed = (double)(end - start) / CLOCKS_PER_SEC;
+    printf("Executed %d iterations of AXPBY on %d elements in %.4f seconds.\n", ITERATIONS, N, elapsed);
+
+    free(x);
+    free(y);
+    return 0;
+}
